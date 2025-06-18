0d197eac2b173d2dc6da3a972b96ecc12e1e0b5d
rehabkanaki <rehabkanaki@gmail.com>
2025-06-18 15:34:07 +0200

    Create run.py

---
+++ run.py
@@ -0,0 +1,13 @@
+import threading
+import asyncio
+from keep_alive import app, application
+
+def run_flask():
+    app.run(host="0.0.0.0", port=10000)
+
+threading.Thread(target=run_flask).start()
+
+asyncio.run(application.initialize())
+asyncio.run(application.start())
+
+asyncio.get_event_loop().run_forever()
