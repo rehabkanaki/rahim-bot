3776a4c40e9da95b564eb469c9f91444fb2dd302
rehabkanaki <rehabkanaki@gmail.com>
2025-06-18 19:10:16 +0200

    Update run.py

--- run.py
+++ run.py
@@ -5,9 +5,13 @@ from keep_alive import app, application
 def run_flask():
     app.run(host="0.0.0.0", port=10000)
 
+# تشغيل Flask في خلفية
 threading.Thread(target=run_flask).start()
 
-asyncio.run(application.initialize())
-asyncio.run(application.start())
+# تشغيل بوت التليقرام
+async def main():
+    await application.initialize()
+    await application.start()
+    await application.updater.start_polling()  # احتياطًا للتأكد من الاتصال
 
-asyncio.get_event_loop().run_forever()
+asyncio.run(main())
