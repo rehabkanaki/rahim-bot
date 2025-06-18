ccfc25b314d95abffad412e3785518d14adb6da3
rehabkanaki <rehabkanaki@gmail.com>
2025-06-18 14:50:06 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,9 +1,23 @@
-from flask import Flask
+import os
+from flask import Flask, request
+from telegram import Update
+from telegram.ext import Application, Dispatcher, ContextTypes
+from main import get_application  # نستورد التطبيق من main
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 app = Flask(__name__)
+application = get_application()  # نحصل على التطبيق من main
+dispatcher: Dispatcher = application.dispatcher
+
 
 @app.route('/')
 def home():
-    return "رحيم شغال 😎"
+    return "Bot is running ✅", 200
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
+
+@app.route(f'/{BOT_TOKEN}', methods=['POST'])
+async def webhook() -> str:
+    update = Update.de_json(request.get_json(force=True), application.bot)
+    await dispatcher.process_update(update)
+    return "ok", 200
