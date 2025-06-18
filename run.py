4748400f2fd37d0587e935e7bbf1ea286f75b798
rehabkanaki <rehabkanaki@gmail.com>
2025-06-18 20:15:22 +0200

    Update run.py

--- run.py
+++ run.py
@@ -5,15 +5,8 @@ from keep_alive import app, application
 def run_flask():
     app.run(host="0.0.0.0", port=10000)
 
-# شغل Flask في خلفية
 threading.Thread(target=run_flask).start()
 
-# شغل البوت
-async def run_bot():
-    await application.initialize()
-    await application.start()
-    # نخلي البوت ينتظر التحديثات اللي بتيجي من التليقرام
-    await application.updater.start_polling()
-    await application.updater.idle()
-
-asyncio.run(run_bot())
+asyncio.run(application.initialize())
+asyncio.run(application.start())
+asyncio.get_event_loop().run_forever()
