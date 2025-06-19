a3115f321543f28789341e38bec92a2866fd346c
rehabkanaki <rehabkanaki@gmail.com>
2025-06-19 11:48:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,5 +23,6 @@ if __name__ == "__main__":
     app.run_webhook(
         listen="0.0.0.0",
         port=int(os.environ.get("PORT", 8443)),
-        webhook_url=f"{APP_URL}/webhook"
+        webhook_url=f"{APP_URL}/webhook",
+        webhook_path="/webhook"  # مهم جداً عشان يشتغل Webhook صح
     )
